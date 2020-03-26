39e4f42f41c77e9d3dc36425a5f604552c768788
NiVanc <nico@izebra.be>
2020-03-26 20:43:49 +0100

    Understand Vue data & binding to attributes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
   el: "#app",
   data: {
-    title: "Hello World!"
+    title: "Hello World!",
+    link: "http://zebra.be",
+    htmlLink: `<a href="http://zebra.be">Zebra</a>`
+  },
+  methods: {
+    sayHello() {
+      this.title = "Welcome and pay a visit to ";
+      return this.title;
+    }
   }
 });
