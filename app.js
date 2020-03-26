5ccedb3a73e9328dd872f71c960bc06aa9ee5635
NiVanc <nico@izebra.be>
2020-03-26 21:16:16 +0100

    Understand Vue events

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ new Vue({
   data: {
     title: "Hello World!",
     link: "http://zebra.be",
-    htmlLink: `<a href="http://zebra.be">Zebra</a>`
+    htmlLink: `<a href="http://zebra.be">Zebra</a>`,
+    counter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
     sayHello() {
       this.title = "Welcome and pay a visit to ";
       return this.title;
+    },
+    increaseCounter(step, event) {
+      this.counter += step;
+    },
+    updateCoordinates(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe() {
+      alert("Watch out, you pressed enter.");
     }
   }
 });
