f843edefeea16f3cc8ec99906c8f71f1edfe7624
NiVanc <nico@izebra.be>
2020-03-29 20:05:53 +0200

    Understand Vue list renderings

--- app2.js
+++ app2.js
@@ -1,6 +1,11 @@
 new Vue({
   el: "#app",
   data: {
-    show: true
+    show: true,
+    ingredients: ["Milk", "Eggs", "Butter", "Flour", "Salt"],
+    persons: [
+      { name: "Nico", age: 33, gender: "Male" },
+      { name: "Deborah", age: 33, gender: "Female" }
+    ]
   }
 });
